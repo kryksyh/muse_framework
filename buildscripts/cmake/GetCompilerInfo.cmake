fff6cac6a79d19dda6c6b30a7957a54b02f70c90
Igor Korsukov <igor.korsukov@gmail.com>
2020-12-03 17:04:20 +0200

    rework root cmake project
    https://github.com/musescore/MuseScore/pull/7025

---
+++ buildscripts/cmake/GetCompilerInfo.cmake
@@ -0,0 +1,32 @@
+
+# Compiler definition
+# MuseScore-specific, defines typical configurations
+
+include(GetPlatformInfo)
+
+if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+
+    set(COMPILER_IS_CLANG 1)
+
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+
+    if (PLATFORM_IS_WINDOWS)
+
+        set(COMPILER_IS_MINGW 1)
+
+    else(PLATFORM_IS_WINDOWS)
+
+        set(COMPILER_IS_GCC 1)
+
+    endif(PLATFORM_IS_WINDOWS)
+
+elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+
+  set(COMPILER_IS_MSVC 1)
+
+endif()
+
+# Define MINGW for VS, as it appears not to be defined
+if (MSVC)
+   set (MINGW false)
+endif (MSVC)
